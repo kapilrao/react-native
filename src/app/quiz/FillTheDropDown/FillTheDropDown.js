28df4a3c13af9a3dc1d9dc910cbcd460e44d92f1
kapil rao <raokapil744@gmail.com>
2019-09-03 20:52:44 +0530

    done

--- src/app/quiz/FillTheDropDown/FillTheDropDown.js
+++ src/app/quiz/FillTheDropDown/FillTheDropDown.js
@@ -1,7 +1,8 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Picker } from 'react-native';
 import React, { Component } from 'react';
 import QuestionSkillScoreComponent from '../QuestionSkillScoreComponent';
 import { FontAwesome } from '@expo/vector-icons';
+import { WebView } from 'react-native';
 
 const styles = StyleSheet.create({
 });
@@ -16,6 +17,10 @@ class FillTheDropDown extends Component {
     }
   }
 
+  renderHtml = quesPartsArray => {
+    console.log('quesPartsArray', quesPartsArray);
+    return '<div>Hello world</div>';
+  }
   render() {
     const { score } = this.state;
     const { question, classes, currentQuestionNo, totalQuestions } = this.props;
@@ -34,6 +39,22 @@ class FillTheDropDown extends Component {
             <Text style={[classes.scorePoints]}>0/1</Text>
             <FontAwesome name={'lightbulb-o'} style={classes.hintBulb} />
           </View>
+        </View>
+        <View style={{ paddingTop: 20, flexDirection: 'row', display: 'flex', alignItems: 'flex-start', justifyContent: 'flex-start' }}>{question.questionParts.map((item, index) => (item.type == 'question' ? (<WebView
+          key={index}
+          originWhitelist={["*"]}
+          source={{ html: `${item.question}` }}
+          style={{ backgroundColor: '#D3D3D3' }}
+        />) : (<Picker
+          key={index}
+          selectedValue={this.state.language}
+          style={{ minWidth: 100, display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}
+          onValueChange={(itemValue, itemIndex) =>
+            this.setState({ language: itemValue })
+          }>
+          {item.values.map((subitems, i) => (<Picker.Item key={i} label={subitems.toUpperCase()} value={subitems} />))}
+        </Picker>)))}
+
         </View>
       </View>
     );
